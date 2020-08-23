517308853772d34d640fcc2b22ad6555bc2b2628
Marijn <31892254+MarijnOomens@users.noreply.github.com>
2020-08-23 12:39:39 +0200

    more in node

--- SuperCircuit/SuperCircuit/Models/Node.cs
+++ SuperCircuit/SuperCircuit/Models/Node.cs
@@ -10,21 +10,39 @@ namespace SuperCircuit.Models
 
         public List<Node> outputNodes = new List<Node>();
 
+        public int InputCount = 0;
+
         public abstract NodeValue CalculateOutput(NodeValue value);
 
         public void Execute(NodeValue value)
         {
-            if(value == NodeValue.None)
+            if(value != NodeValue.Off && value != NodeValue.On)
             {
                 return;
             }
 
             inputValues.Add(value);
+
+            if (InputCount == inputValues.Count)
+            {
+                CalculateOutput(value);
+
+                foreach (var node in outputNodes)
+                {
+                    node.Execute(value);
+                }
+            }
         }
 
-        public void AddOutpu(Node output)
+        public void AddOutput(Node output)
         {
             outputNodes.Add(output);
+            output.IncreaseInput();
+        }
+
+        public void IncreaseInput()
+        {
+            InputCount += 1;
         }
     }
 }
